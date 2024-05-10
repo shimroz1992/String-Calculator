f5647746e37a5c78ba53f8658b28a15823c07e12
shimroz ahemed <shimrozk92@gmail.com>
2024-05-10 19:57:50 +0530

    Handle the case where the input string contains two numbers separated by a comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe StringCalculator do
     it "returns the number itself for a single number" do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it "returns the sum of two numbers" do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
   end
 end
